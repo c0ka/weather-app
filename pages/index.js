b24021ca5dfe7478854de4f3fc425f29ca1b1572
Julian Hu <julianhy@gmail.com>
2021-11-30 13:56:58 +0800

    api test and useEffect

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,34 @@
-export default function index() {
+import { useEffect, useState } from "react";
+
+import styles from "../styles/Home.module.css"
+
+const App = () => {
+  const [cityInput, setCityInput] = useState("Nanjing");
+  const [triggerFetch, setTriggerFetch] = useState(true);
+  const [weatherData, setWeatherData] = useState();
+  const [unitSystem, setUnitSystem] =useState("metric");
+
+  useEffect(() => {
+    const getData= async () => {
+      const res = await fetch("api/data", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ cityInput }),
+      });
+      const data = await res.json();
+      setWeatherData({...data});
+      setCityInput("");
+    };
+    getData();
+  }, [triggerFetch]);
+
+  console.log(weatherData);
+
   return (
-    <h1>Hello World!</h1>
-  )
-}
+    <div className={styles.wrapper}>
+      <p>Weather App Wrapper</p>
+    </div>
+  );
+};
+
+export default App;
